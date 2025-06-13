750e581495669ccb39f57ca057ef7766d86ad766
alves-dev <alvesmoreiraigor@gmail.com>
2025-06-13 07:22:36 -0300

    feat: defines setting.py for envs

---
+++ app/config/setting.py
@@ -0,0 +1,16 @@
+import dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # GITHUB
+    GITHUB_BASE_CLONE_URL: str = 'https://github.com'
+    GITHUB_TOKEN: str = None
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+dotenv.load_dotenv()
+setting = Settings()
