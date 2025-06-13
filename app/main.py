3e14351051a54b5b38d324111ad398ff31be3547
alves-dev <alvesmoreiraigor@gmail.com>
2025-06-13 06:55:41 -0300

    feat: main.py created

---
+++ app/main.py
@@ -0,0 +1,28 @@
+import argparse
+import sys
+
+from app.github.client import GitRepository
+from app.readme import genetare
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Script que recebe argumento do repositório")
+    parser.add_argument("repository", help="Caminho local ou URL do repositório")
+    args = parser.parse_args()
+
+    print(f"Repositório recebido: {args.repository}")
+
+    repo = GitRepository(args.repository)
+    files_ok = genetare.check_files(repo.repo_path)
+    if not files_ok:
+        print("Repository doesn't have .docs or .docs/data.yaml")
+        exit()
+
+    new_readme = genetare.full_readme(repo.repo_path)
+    repo.write_file('README.md', new_readme)
+    repo.commit_and_push('New readme generated')
+
+
+if __name__ == "__main__":
+    sys.argv = ["main.py", "alves-dev/posts"]
+    main()
