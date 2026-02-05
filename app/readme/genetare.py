daab528afe3a7cae2f033934cf5d4340da10657c
alves-dev <alvesmoreiraigor@email.com>
2026-02-05 18:46:39 -0300

    feat: enhance README generation with tech stack badges and templates

--- app/readme/genetare.py
+++ app/readme/genetare.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from datetime import datetime
 from pathlib import Path
 from string import Template
@@ -6,6 +7,9 @@ from app.utils import folder_files as files
 from app.utils import repo_data
 
 BASE_PATH_TEMPLATES = str(Path(__file__).parent.parent.parent.resolve()) + '/templates'
+
+BADGE_HEADER_COLOR = '10B981'  # Verde primário
+BADGE_NONE_COLOR = '64748B'  # Cinza escuro
 
 
 def full_readme(path: Path) -> str:
@@ -16,9 +20,11 @@ def full_readme(path: Path) -> str:
     base = files.load_base_md(path)
 
     header = _generate_head(path)
+    stack = _generate_stack(path)
     footer = _generate_footer(path)
 
     base = base.replace('<!-- TEMPLATE:header -->', header)
+    base = base.replace('<!-- TEMPLATE:stack -->', stack)
     base = base.replace('<!-- TEMPLATE:footer -->', footer)
 
     return base
@@ -36,11 +42,11 @@ def _generate_head(path: Path) -> str:
     if 'code' == repo_type:
         template_name = 'code'
 
-    head_template: str = f"{BASE_PATH_TEMPLATES}/header-{template_name}-{head_version}.md"
+    template_path: str = f"{BASE_PATH_TEMPLATES}/header-{template_name}-{head_version}.md"
 
     head_data['topics_tags_badges'] = _build_topics_badges(head_data['topics'])
 
-    return _render_template(head_template, head_data)
+    return _render_template(template_path, head_data)
 
 
 def _generate_footer(path: Path) -> str:
@@ -50,12 +56,45 @@ def _generate_footer(path: Path) -> str:
     template_name = 'default'
 
     footer_data: dict = _footer_data(metadata)
-    footer_template: str = f"{BASE_PATH_TEMPLATES}/footer-{template_name}-{template_version}.md"
+    template_path: str = f"{BASE_PATH_TEMPLATES}/footer-{template_name}-{template_version}.md"
 
     footer_data['license'] = _format_tag(footer_data['license'])
     footer_data['datetime'] = str(datetime.now().strftime("%Y-%m-%d %H:%M"))
 
-    return _render_template(footer_template, footer_data)
+    return _render_template(template_path, footer_data)
+
+
+def _generate_stack(path: Path) -> str:
+    metadata = files.load_repository_metadata(path)
+
+    version: str = 'v1'
+    template_name = 'default'
+    template_path: str = f"{BASE_PATH_TEMPLATES}/stack-{template_name}-{version}.md"
+
+    language: str = repo_data.get_project_language(metadata)
+    framework: str = repo_data.get_project_framework(metadata)
+    database: str = repo_data.get_project_database(metadata)
+    protocols: list = repo_data.get_project_protocols(metadata)
+    tools: list = repo_data.get_project_tools(metadata)
+
+    framework_badge = _badge_header('Framework') + _badges(framework, '10B981')
+    if framework is None:
+        framework_badge = _badge_header('Framework') + _badges('no-framework', BADGE_NONE_COLOR)
+
+    database_badge = _badges(database, '10B981')
+    if database is None:
+        database_badge = _badges('no-database', BADGE_NONE_COLOR)
+
+    data: dict = {
+        'language_badge': _badge_header('Language') + _badges(language, '3B82F6'),
+        'framework_badge': framework_badge,
+        'database_badge': database_badge,
+        'protocols_badges': _badges(protocols, '6366F1'),
+        'tools_badges': _badges(tools, '0EA5E9'),
+        'protocols_header': _badge_header('Protocols')
+    }
+
+    return _render_template(template_path, data)
 
 
 def _format_tag(tag: str) -> str:
@@ -130,8 +169,26 @@ def _build_topics_badges(topics: list[str]) -> str:
         for tag in topics
     )
 
-    header = (
-        '<img src="https://img.shields.io/badge/topics-10B981?style=flat-square&logoColor=white"/>'
-    )
+    return f'{_badge_header('topics')} {badges}'
 
-    return f'{header} {badges}'
+
+def _badge_header(title: str) -> str:
+    return f'<img src="https://img.shields.io/badge/{title}-{BADGE_HEADER_COLOR}?style=flat-square&logoColor=white"/>'
+
+
+def _badges(items, color='3B82F6'):
+    if not items:
+        return '---'
+
+    # Se for string, transforma em lista
+    if isinstance(items, str):
+        items = [items]
+
+    # Segurança extra: evita iterar char por char
+    if not isinstance(items, Iterable):
+        items = [str(items)]
+
+    return ' '.join(
+        f'<img src="https://img.shields.io/badge/{_format_tag(item)}-{color}?style=flat-square"/>'
+        for item in items
+    )
